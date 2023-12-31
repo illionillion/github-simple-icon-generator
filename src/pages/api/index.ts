489df61e19b42407bf19f4a6b81e467945b54ba0
yuto ichinose <yuto.0526.sano@gmail.com>
2024-01-01 00:37:26 +0900

    fix: try catch

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -14,62 +14,68 @@ export default async function handler(
     return res.status(400).json({ message: `400 Bad Request.` });
   }
 
-  const github = await fetch(`https://api.github.com/users/${username}`);
-  const json: any = await github.json();
-  if (!Object.keys(json).includes('avatar_url')) {
-    return res.status(400).json({ message: `400 Bad Request.` });
-  }
-  const icon = await fetch(json.avatar_url);
-  const iconBuffer = await icon.arrayBuffer();
-
-  // アイコン画像をリサイズ
-  const resizedIconBuffer = await sharp(iconBuffer)
-    .resize(size, size) // リサイズ
-    .composite([
-      {
-        input: Buffer.from(
-          `<svg><circle cx="${size / 2}" cy="${size / 2}" r="${size / 2}" fill="white"/></svg>`
-        ),
-        top: 0,
-        left: 0,
-        blend: "dest-in", // マスキングを適用,
-        limitInputPixels: false
-      },
-    ])
-    .png()
-    .toBuffer();
-
-  const buffer = await sharp({
-    create: {
-      width: size,
-      height: size,
-      channels: 3,
-      background: !!bgColor ? bgColor : { r: 255, g: 100, b: 100, alpha: 0.5 },
-    },
-  })
-    .composite([
-      {
-        input: resizedIconBuffer, // リサイズしたアイコン画像を使う
-        blend: "over",
-        top: 0,
-        left: 0,
-        
-      },
-    ])
-    .png()
-    .toBuffer();
-
-  res.setHeader("Content-Type", "image/png");
-  res.setHeader("Content-Disposition", "inline;");
-  res.status(200).send(buffer);
-}
-
-
-const sizeCheck = (size: string):number => {
-    if (isNaN(parseInt(size)) || parseInt(size) === 0) {
-        return 100
-    } else if (parseInt(size) > 10000) {
-        return 10000
+  try {
+    const github = await fetch(`https://api.github.com/users/${username}`);
+    const json: any = await github.json();
+    if (!Object.keys(json).includes("avatar_url")) {
+      return res.status(400).json({ message: `400 Bad Request.` });
     }
-    return parseInt(size)
+    const icon = await fetch(json.avatar_url);
+    const iconBuffer = await icon.arrayBuffer();
+
+    // アイコン画像をリサイズ
+    const resizedIconBuffer = await sharp(iconBuffer)
+      .resize(size, size) // リサイズ
+      .composite([
+        {
+          input: Buffer.from(
+            `<svg><circle cx="${size / 2}" cy="${size / 2}" r="${
+              size / 2
+            }" fill="white"/></svg>`
+          ),
+          top: 0,
+          left: 0,
+          blend: "dest-in", // マスキングを適用,
+          limitInputPixels: false,
+        },
+      ])
+      .png()
+      .toBuffer();
+
+    const buffer = await sharp({
+      create: {
+        width: size,
+        height: size,
+        channels: 3,
+        background: !!bgColor
+          ? bgColor
+          : { r: 255, g: 100, b: 100, alpha: 0.5 },
+      },
+    })
+      .composite([
+        {
+          input: resizedIconBuffer, // リサイズしたアイコン画像を使う
+          blend: "over",
+          top: 0,
+          left: 0,
+        },
+      ])
+      .png()
+      .toBuffer();
+
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Content-Disposition", "inline;");
+    res.status(200).send(buffer);
+  } catch (error) {
+    return res.status(400).json({ message: `400 Bad Request.`, error: error });
+  }
 }
+
+const sizeCheck = (size: string): number => {
+  if (isNaN(parseInt(size)) || parseInt(size) === 0) {
+    return 100;
+  } else if (parseInt(size) > 10000) {
+    return 10000;
+  }
+  return parseInt(size);
+};
